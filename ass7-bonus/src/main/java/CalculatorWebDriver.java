86e06c19094883348146b9a1ec4286bf2f9f851a
ryandunn399 <ryandunn399@gmail.com>
2023-10-31 14:44:43 +0000

    Added the ability to directly test the online calculator using selenium.

---
+++ ass7-bonus/src/main/java/CalculatorWebDriver.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriver {
+
+    private WebDriver driver;
+
+    /**
+     * Class constructor. Will initialize a chrome driver.
+     */
+    public WebDriver() {
+        WebDriver driver = new ChromeDriver();
+    }
+
+}
